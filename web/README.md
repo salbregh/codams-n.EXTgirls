e6c83ddce40d74aa429a4cd4cddbbe26d320b54e
Mares Verbrugge <maresverbrugge@MacBook-Pro-van-Mares.local>
2021-11-11 14:51:24 +0100

    map

---
+++ web/README.md
@@ -0,0 +1,4 @@
+# tomtom-use-case-store-locator
+
+Store locator is a sample application based on <a href="https://developer.tomtom.com/maps-sdk-web/tutorials-use-cases/store-locator">Store Locator Tutorial</a>. 
+The tutorial shows you how to build your company's stores locator. You can choose an address from a side menu to see where an exact store is located on a map. Alternatively, you can select the store on the map to see its details in the side menu.
---
+++ web/stores.js
@@ -0,0 +1,181 @@
+const stores = {
+  "type": "FeatureCollection",
+  "features": [{
+      "type": "Feature",
+      "geometry": {
+          "type": "Point",
+          "coordinates": [
+              4.91627,
+              52.3718
+          ]
+      },
+      "properties": {
+          "address": "Codam Coding College, Amsterdam, The Netherlands",
+          "city": "Starting Point - we need a holiday",
+          "address": "Codam Coding College, Amsterdam, The Netherlands",
+          "facility": "Facility rating: 5/5",
+          "safety": "Safety rating: 5/5"
+      }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [
+          2.79586,
+          49.76672
+        ]
+    },
+    "properties": {
+      "address": "80700 Hattencourt, France",
+      "city": "Rest stop option Hattencourt",
+      "facility": "Facility rating: 1/5",
+      "safety": "Safety rating: 4/5"
+    }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+          "type": "Point",
+          "coordinates": [
+            2.85226,
+            49.94223
+          ]
+      },
+      "properties": {
+        "address": "80200 A. du N, 80200 Feuillères, France",
+        "city": "Rest stop option Feuillères",
+        "facility": "Facility rating: 2/5",
+        "safety": "Safety rating: 4/5"
+      }
+    },
+      {
+          "type": "Feature",
+          "geometry": {
+              "type": "Point",
+              "coordinates": [
+                1.81374,
+                48.22406
+              ]
+          },
+          "properties": {
+            "address": "28310 Poinville, France",
+            "city": "Rest stop option Poinville",
+            "facility": "Facility rating: 4/5",
+            "safety": "Safety rating: 2/5"
+          }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+              1.79995,
+              48.15143
+            ]
+        },
+        "properties": {
+          "address": "28140 Tillay-le-Péneux, France",
+          "city": "Rest stop option Tillay-le-Péneux",
+          "facility": "Facility rating: 2/5",
+          "safety": "Safety rating: 4/5"
+        }
+    },
+      {
+          "type": "Feature",
+          "geometry": {
+              "type": "Point",
+              "coordinates": [
+                1.47272,
+                44.79511
+              ]
+          },
+          "properties": {
+            "address": "Av. de Toulouse, 46350 Payrac, France",
+            "city": "Rest stop option Payrac",
+            "facility": "Facility rating: 1/5",
+            "safety": "Safety rating: 3/5"
+          }
+      },
+      {
+          "type": "Feature",
+          "geometry": {
+              "type": "Point",
+              "coordinates": [
+                1.87958,
+                44.49152
+              ]
+          },
+          "properties": {
+            "address": "Larnagol Street, 46160 Larnagol, France",
+            "city": "Rest stop option Larnagol",
+            "facility": "Facility rating: 3/5",
+            "safety": "Safety rating: 1/5"
+          }
+      },
+      {
+          "type": "Feature",
+          "geometry": {
+              "type": "Point",
+              "coordinates": [
+                1.89233,
+                44.85939
+                ]
+          },
+          "properties": {
+            "address": "13 Av. François de Maynard, 46400 Saint-Céré, France",
+            "city": "Rest stop option Saint-Céré",
+            "facility": "Facility rating: 4/5",
+            "safety": "Safety rating: 5/5"
+          }
+      },
+      {
+          "type": "Feature",
+          "geometry": {
+              "type": "Point",
+              "coordinates": [
+                1.37213,
+                44.51211
+                ]
+          },
+          "properties": {
+            "address": "46000 Cahors, France",
+            "city": "Rest stop option Cahors",
+            "facility": "Facility rating: 5/5",
+            "safety": "Safety rating: 2/5"
+          }
+      },
+      {
+          "type": "Feature",
+          "geometry": {
+              "type": "Point",
+              "coordinates": [
+                1.44147,
+                44.44563
+                ]
+          },
+          "properties": {
+            "address": "D14, 46500 Gramat, France",
+            "city": "Rest stop option Gramat",
+            "facility": "Facility rating: 3/5",
+            "safety": "Safety rating: 2/5"
+          }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+              2.17484,
+              41.40319
+              ]
+        },
+        "properties": {
+          "address": "Sagrada Familia, Barcelona, Spain",
+          "city": "Final Destination :) ",
+          "facility": "Facility rating: 3/5",
+          "safety": "Safety rating: 4/5"
+        }
+    }
+  ]
+};
